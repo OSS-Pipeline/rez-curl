b3906ec59b981adb822fe1b208bae24652574cab
Joshua Senouf <jsenouf@MTLws032.mtloutpost.com>
2019-10-08 14:21:46 -0400

    Adding support for PKG_CONFIG_PATH

--- package.py
+++ package.py
@@ -35,6 +35,7 @@ uuid = "curl-{version}".format(version=str(version))
 def commands():
     env.PATH.prepend("{root}/bin")
     env.LD_LIBRARY_PATH.prepend("{root}/lib")
+    env.PKG_CONFIG_PATH.prepend("{root}/lib/pkgconfig")
 
     # Helper environment variables.
     env.CURL_BINARY_PATH.set("{root}/bin")
